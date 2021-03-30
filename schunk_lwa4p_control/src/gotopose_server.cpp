f48752102a2d36d84d74856f8035b2d644ac209d
Filip Zoric <fzoric8@gmail.com>
2021-03-30 18:57:16 +0200

    Added initial skeleton for action server

--- schunk_lwa4p_control/src/gotopose_server.cpp
+++ schunk_lwa4p_control/src/gotopose_server.cpp
@@ -1,35 +1,50 @@
-#include "control_arm/gotopose_server.h"
-#include "schunk_lwa4p_control/GoToPoseAction.h"
+#include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+#include <schunk_lwa4p_control/GoToPoseAction.h>
 
 
-GoToPoseActionServer::GoToPoseActionServer(std::string name) :
-    as_(nh_, name, boost::bind(&GoToPoseActionServer::executeCB, this, _1), false),
-    action_name_(name)
+class GoToPoseActionServer{
+    protected:
+        ros::NodeHandle nh_;
+        actionlib::SimpleActionServer<schunk_lwa4p_control::GoToPoseAction> as_;
+        std::string action_name_;
+        schunk_lwa4p_control::GoToPoseFeedback feedback_;
+        schunk_lwa4p_control::GoToPoseResult result_;
+
+    public:
+
+    GoToPoseActionServer(std::string name) :
+        as_(nh_, name, boost::bind(&GoToPoseActionServer::executeCB, this, _1), false),
+        action_name_(name)
     {
         as_.start();
     }
 
-GoToPoseActionServer::~GoToPoseActionServer(void)
-{
+    ~GoToPoseActionServer(void)
+    {
 
-}
+    }
 
-void executeCB(const schunk_lwa4p_control::GoToPoseGoalConstPtr &goal)
-{
-    //helper variables
-    ros::Rate r(1);
-    bool success = true;
+    void initializeSubscribers()
+    {
+        // Init subscriber for current state (arm)
+    }
 
-    // set data to feedback
+    void executeCB(const schunk_lwa4p_control::GoToPoseGoalConstPtr &goal)
+    {
+        ros::Rate(1);
+        bool success = false;
+    }
 
-}
+
+};
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "go_to_pose");
+    ros::init(argc, argv, "gotopose");
 
-    GoToPoseAction gotoposeaction("go_to_pose");
+    GoToPoseActionServer gotopose("go_to_pose");
     ros::spin();
-
     return 0;
+
 }
