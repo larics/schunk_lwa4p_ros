9df71720b2ada8b8d0da5a31835fbe3d86a70426
Filip Zoric <fzoric8@gmail.com>
2021-04-08 14:33:14 +0200

    Added updated SetupDistancer action

--- schunk_lwa4p_control/src/setupdistancer_server.cpp
+++ schunk_lwa4p_control/src/setupdistancer_server.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <actionlib/server/simple_action_server.h>
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Point.h>
 #include <std_srvs/Trigger.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Matrix3x3.h>
@@ -16,6 +17,7 @@ protected:
 
     // publishers
     ros::Publisher cmdPosePublisher;
+    ros::Publisher cmdOrientationPublisher;
 
     // subscribers
     ros::Subscriber currentPoseSubscriber;
@@ -33,6 +35,10 @@ protected:
     // msgs
     geometry_msgs::Pose currentPose;
     geometry_msgs::Pose cmdPose;
+    geometry_msgs::Point cmdOrientation;
+
+    // wanted tool rotations
+    tf2Scalar roll; tf2Scalar pitch; tf2Scalar yaw;
 
 
 public:
@@ -48,9 +54,10 @@ public:
         initializeServices();
         as_.start();
         ROS_INFO("[SetupDistancerServer] Initialized...");
-        // Add some collisions
+        // Add some collisions (table for planning)
         std_srvs::Trigger coll_srv;
         addCollisionsServiceClient.call(coll_srv);
+
 
     }
 
@@ -69,6 +76,7 @@ public:
     void initializePublishers()
     {
         cmdPosePublisher = nh_.advertise<geometry_msgs::Pose>("/control_arm_node/arm/command/pose", 1);
+        cmdOrientationPublisher = nh_.advertise<geometry_msgs::Point>("/control_arm_node/tool/command/orientation", 1);
     }
 
     void initializeServices()
@@ -86,7 +94,6 @@ public:
         currentPose.position = msg->position;
         currentPose.orientation = msg->orientation;
 
-        tf2Scalar roll; tf2Scalar pitch; tf2Scalar yaw;
         tf2::Quaternion quaternion(currentPose.orientation.x, currentPose.orientation.y, currentPose.orientation.z, currentPose.orientation.w);
 
         // Get quaternion from ROS msg into tf2 quaternion
@@ -101,8 +108,6 @@ public:
 
     }
 
-
-
     void executeCB(const schunk_lwa4p_control::SetupDistancerGoalConstPtr &goal)
     {
 
@@ -115,14 +120,46 @@ public:
         bool reached = false;
         bool preempted = false;
         int tRecvGoal = ros::Time::now().toSec();
+        int timeout = goal->timeout_sec;
         r.sleep();
 
+        while (!elapsed) {
+            r.sleep();
+            elapsed = (ros::Time::now().toSec() - tRecvGoal) > timeout;
+
+            // Remove collisions for tool
+            ROS_INFO("[SetupDistancerServer] Removing tool collisions..");
+            std_srvs::Trigger srv;
+            disableToolCollisionsServiceClient.call(srv);
+
+            ROS_INFO("[SetupDistancerServer] Starting tool rotation.");
+            cmdOrientation = goal->goal_orientation;
+            cmdOrientationPublisher.publish(cmdOrientation);
+
+            // This condition may fail because of type comparison, check how to transform tf2scalar to float
+            while (cmdOrientation.z !=  yaw){
+                ROS_INFO("[SetupDistancerServer] Rotating tool...");
+            }
+
+            ROS_INFO("[SetupDistancerServer] Rotation complete.");
+
+            if (as_.isPreemptRequested() || !ros::ok())
+            {
+                ROS_INFO("%s: Preempted", action_name_.c_str());
+                // set the action state to preempted
+                as_.setPreempted();
+                reached = false;
+                preempted = true;
+            }
+        }
+
         // TODO:
-        // 1. Build new action and start new action server
-        // 2. Command tool service based on received goal
+        // 1. Build new action and start new action server --> DONE
+        // 2. Command tool service based on received goal --> DONE
         // 3. Command tool orientation based on received goal
         // 4. Create sequence of service calls (remove_collision, send_orientation, close motors)
         // 5. Check orientation in which format it's written (radians/degrees)
+        // Think of arm servoing as a service moveit::servo as service that can be called during executing of this server
 
 
         /*
@@ -138,6 +175,10 @@ public:
         int timeout = goal->timeout_sec;
 
         //
+
+        while (!elapsed) {
+            r
+        }
 
         while (checkDist(goalOrientation, currentOrientation) and
         while (checkDist(cmdPose, currentPose) > epsilon && !elapsed){
